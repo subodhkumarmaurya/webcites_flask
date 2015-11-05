dd9cd2ea723d35af535ba052ec4bbce72752fe60
Becky Smethurst <becky@MacBook-Air-2.local>
2015-11-05 21:22:27 +1100

    new geocode function

--- geocode.py
+++ geocode.py
@@ -8,10 +8,17 @@ from urllib2 import urlopen
 import requests
 
 def geocode(address):
-	url = (u"http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address={0}".format(address.replace(" ", "+")))
-	result = requests.get(url).json()
-	if result["status"]==u'OK':
-		res = result.get("results")[0]
-		return res["geometry"]["location"]["lat"], res["geometry"]["location"]["lng"]
-	else:
-		pass
+	#url = (u"http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address={0}".format(address.replace(" ", "+")))
+	query_terms = dict()
+	query_terms["key"]="Ai4Hluuw58D5pakGOmh5zwFr4yLY-bN7E2EGnJc2aMxbUUdJPzlRbg8kjiWMtrFP"
+	query_terms["q"] = address
+	#url = (u"http://dev.virtualearth.net/REST/v1/Locations?".format(address.replace(" ", "+"))."&key=BingMapsKey")
+	url = "http://dev.virtualearth.net/REST/v1/Locations"
+	result = requests.get(url, params=query_terms).json()
+	try:
+		if result["resourceSets"][0]["estimatedTotal"] > 0:
+			return result["resourceSets"][0]["resources"][0]["geocodePoints"][0]["coordinates"]
+	except:
+		return None
+
+	return None
